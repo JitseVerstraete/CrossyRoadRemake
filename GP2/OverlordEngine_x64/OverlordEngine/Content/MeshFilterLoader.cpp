b9fac010631e48015b109edfd61a5477bd0767e5
Jitse Verstraete <jitse.verstraete@gmail.com>
2022-04-20 18:02:58 +0200

    hardware skinning almost done
    still getting two warnings in the console and some memory leaks (ask in class)

--- GP2/OverlordEngine_x64/OverlordEngine/Content/MeshFilterLoader.cpp
+++ GP2/OverlordEngine_x64/OverlordEngine/Content/MeshFilterLoader.cpp
@@ -4,9 +4,9 @@
 //#define MULTI_TEXCOORD
 #define OVM_vMAJOR 1
 #ifdef MULTI_TEXCOORD
-	#define OVM_vMINOR 61
+#define OVM_vMINOR 61
 #else
-	#define OVM_vMINOR 1
+#define OVM_vMINOR 1
 #endif
 
 
@@ -15,7 +15,7 @@ MeshFilter* MeshFilterLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	const auto pReader = new BinaryReader();
 	pReader->Open(loadInfo.assetFullPath);
 
-	if(!pReader->Exists())
+	if (!pReader->Exists())
 		return nullptr;
 
 	//READ OVM FILE
@@ -38,138 +38,187 @@ MeshFilter* MeshFilterLoader::LoadContent(const ContentLoadInfo& loadInfo)
 	for (;;)
 	{
 		const auto meshDataType = static_cast<MeshDataType>(pReader->Read<char>());
-		if(meshDataType == MeshDataType::END)
+		if (meshDataType == MeshDataType::END)
 			break;
 
 		const auto dataOffset = pReader->Read<unsigned int>();
 
-		switch(meshDataType)
+		switch (meshDataType)
 		{
 		case MeshDataType::HEADER:
-			{
-				pMesh->m_MeshName = pReader->ReadString();
-				vertexCount = pReader->Read<unsigned int>();
-				indexCount = pReader->Read<unsigned int>();
+		{
+			pMesh->m_MeshName = pReader->ReadString();
+			vertexCount = pReader->Read<unsigned int>();
+			indexCount = pReader->Read<unsigned int>();
 
-				pMesh->m_VertexCount = vertexCount;
-				pMesh->m_IndexCount = indexCount;
-			}
-			break;
+			pMesh->m_VertexCount = vertexCount;
+			pMesh->m_IndexCount = indexCount;
+		}
+		break;
 		case MeshDataType::POSITIONS:
-			{
-				pMesh->SetElement(ILSemantic::POSITION);
+		{
+			pMesh->SetElement(ILSemantic::POSITION);
 
-				for(unsigned int i = 0; i<vertexCount; ++i)
-				{
-					XMFLOAT3 pos{};
-					pos.x = pReader->Read<float>();
-					pos.y = pReader->Read<float>();
-					pos.z = pReader->Read<float>();
-					pMesh->m_Positions.emplace_back(pos);
-				}
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				XMFLOAT3 pos{};
+				pos.x = pReader->Read<float>();
+				pos.y = pReader->Read<float>();
+				pos.z = pReader->Read<float>();
+				pMesh->m_Positions.emplace_back(pos);
 			}
-			break;
+		}
+		break;
 		case MeshDataType::INDICES:
+		{
+			for (unsigned int i = 0; i < indexCount; ++i)
 			{
-				for(unsigned int i = 0; i<indexCount; ++i)
-				{
-					pMesh->m_Indices.emplace_back(pReader->Read<DWORD>());
-				}
+				pMesh->m_Indices.emplace_back(pReader->Read<DWORD>());
 			}
-			break;
+		}
+		break;
 		case MeshDataType::NORMALS:
-			{
-				pMesh->SetElement(ILSemantic::NORMAL);
+		{
+			pMesh->SetElement(ILSemantic::NORMAL);
 
-				for (unsigned int i = 0; i<vertexCount; ++i)
-				{
-					XMFLOAT3 normal{};
-					normal.x = pReader->Read<float>();
-					normal.y = pReader->Read<float>();
-					normal.z = pReader->Read<float>();
-					pMesh->m_Normals.emplace_back(normal);
-				}
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				XMFLOAT3 normal{};
+				normal.x = pReader->Read<float>();
+				normal.y = pReader->Read<float>();
+				normal.z = pReader->Read<float>();
+				pMesh->m_Normals.emplace_back(normal);
 			}
-			break;
+		}
+		break;
 		case MeshDataType::TANGENTS:
-			{
-				pMesh->SetElement(ILSemantic::TANGENT);
+		{
+			pMesh->SetElement(ILSemantic::TANGENT);
 
-				for (unsigned int i = 0; i<vertexCount; ++i)
-				{
-					XMFLOAT3 tangent{};
-					tangent.x = pReader->Read<float>();
-					tangent.y = pReader->Read<float>();
-					tangent.z = pReader->Read<float>();
-					pMesh->m_Tangents.emplace_back(tangent);
-				}
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				XMFLOAT3 tangent{};
+				tangent.x = pReader->Read<float>();
+				tangent.y = pReader->Read<float>();
+				tangent.z = pReader->Read<float>();
+				pMesh->m_Tangents.emplace_back(tangent);
 			}
-			break;
+		}
+		break;
 		case MeshDataType::BINORMALS:
-			{
-				pMesh->SetElement(ILSemantic::BINORMAL);
+		{
+			pMesh->SetElement(ILSemantic::BINORMAL);
 
-				for (unsigned int i = 0; i<vertexCount; ++i)
-				{
-					XMFLOAT3 binormal{};
-					binormal.x = pReader->Read<float>();
-					binormal.y = pReader->Read<float>();
-					binormal.z = pReader->Read<float>();
-					pMesh->m_Binormals.emplace_back(binormal);
-				}
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				XMFLOAT3 binormal{};
+				binormal.x = pReader->Read<float>();
+				binormal.y = pReader->Read<float>();
+				binormal.z = pReader->Read<float>();
+				pMesh->m_Binormals.emplace_back(binormal);
 			}
-			break;
+		}
+		break;
 		case MeshDataType::TEXCOORDS:
-			{
-				pMesh->SetElement(ILSemantic::TEXCOORD);
+		{
+			pMesh->SetElement(ILSemantic::TEXCOORD);
 
-				constexpr auto amountTexCoords = 1;
+			constexpr auto amountTexCoords = 1;
 #ifdef MULTI_TEXCOORD
-				amountTexCoords = pReader->Read<USHORT>();
+			amountTexCoords = pReader->Read<USHORT>();
 #endif
-				pMesh->m_TexCoordCount = amountTexCoords;
-				for (unsigned int i = 0; i<vertexCount*amountTexCoords; ++i)
-				{
-					XMFLOAT2 tc{};
-					tc.x = pReader->Read<float>();
-					tc.y = pReader->Read<float>();
-					pMesh->m_TexCoords.emplace_back(tc);
-				}
-			}
-			break;
-		case MeshDataType::COLORS:
+			pMesh->m_TexCoordCount = amountTexCoords;
+			for (unsigned int i = 0; i < vertexCount * amountTexCoords; ++i)
 			{
-				pMesh->SetElement(ILSemantic::COLOR);
-
-				for (unsigned int i = 0; i<vertexCount; ++i)
-				{
-					XMFLOAT4 color{};
-					color.x = pReader->Read<float>();
-					color.y = pReader->Read<float>();
-					color.z = pReader->Read<float>();
-					color.w = pReader->Read<float>();
-					pMesh->m_Colors.emplace_back(color);
-				}
+				XMFLOAT2 tc{};
+				tc.x = pReader->Read<float>();
+				tc.y = pReader->Read<float>();
+				pMesh->m_TexCoords.emplace_back(tc);
 			}
-			break;
+		}
+		break;
+		case MeshDataType::COLORS:
+		{
+			pMesh->SetElement(ILSemantic::COLOR);
+
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				XMFLOAT4 color{};
+				color.x = pReader->Read<float>();
+				color.y = pReader->Read<float>();
+				color.z = pReader->Read<float>();
+				color.w = pReader->Read<float>();
+				pMesh->m_Colors.emplace_back(color);
+			}
+		}
+		break;
 		case MeshDataType::BLENDINDICES:
 		{
-			TODO_W7(L"Add BLENDINDICES information");
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				XMFLOAT4 indices{};
+				indices.x = pReader->Read<float>();
+				indices.y = pReader->Read<float>();
+				indices.z = pReader->Read<float>();
+				indices.w = pReader->Read<float>();
+				pMesh->m_BlendIndices.emplace_back(indices);
+			}
+			pMesh->m_HasAnimations = true;
 		}
 		break;
 		case MeshDataType::BLENDWEIGHTS:
 		{
-			TODO_W7(L"Add BLENDWEIGHTS information");
+			for (unsigned int i = 0; i < vertexCount; ++i)
+			{
+				XMFLOAT4 weights{};
+				weights.x = pReader->Read<float>();
+				weights.y = pReader->Read<float>();
+				weights.z = pReader->Read<float>();
+				weights.w = pReader->Read<float>();
+				pMesh->m_BlendWeights.emplace_back(weights);
+			}
+			pMesh->m_HasAnimations = true;
+
 		}
 		break;
 		case MeshDataType::ANIMATIONCLIPS:
 		{
-			TODO_W7(L"Add ANIMATIONCLIPS information");
+			uint16_t clipCount{ pReader->Read<uint16_t>() };
+			for (int c = 0; c < clipCount; c++) //for every clip
+			{
+				AnimationClip clip{};
+
+				clip.name = pReader->ReadString();
+				clip.duration = pReader->Read<float>();
+				clip.ticksPerSecond = pReader->Read<float>();
+
+				uint16_t keyCount = pReader->Read<uint16_t>();
+				for (int k = 0; k < keyCount; k++) //for every key
+				{
+					AnimationKey key{};
+					key.tick = pReader->Read<float>();
+
+					uint16_t boneCount = pReader->Read<uint16_t>();
+					for (int b = 0; b < boneCount; ++b) //for every bone
+					{
+						XMFLOAT4X4 boneTransform = pReader->Read<XMFLOAT4X4>();
+						key.boneTransforms.emplace_back(boneTransform);
+					}
+					clip.keys.emplace_back(key);
+				}
+
+				pMesh->m_AnimationClips.emplace_back(clip);
+			}
+			pMesh->m_HasAnimations = true;
+
+
+
 		}
 		break;
 		case MeshDataType::SKELETON:
 		{
-			TODO_W7(L"Add SKELETON information");
+			pMesh->m_BoneCount = pReader->Read<uint16_t>();
+			pMesh->m_HasAnimations = true;
 		}
 		break;
 		default:
--- GP2/OverlordEngine_x64/OverlordEngine/Misc/ModelAnimator.cpp
+++ GP2/OverlordEngine_x64/OverlordEngine/Misc/ModelAnimator.cpp
@@ -1,15 +1,14 @@
 #include "stdafx.h"
 #include "ModelAnimator.h"
 
-ModelAnimator::ModelAnimator(MeshFilter* pMeshFilter):
-	m_pMeshFilter{pMeshFilter}
+ModelAnimator::ModelAnimator(MeshFilter* pMeshFilter) :
+	m_pMeshFilter{ pMeshFilter }
 {
 	SetAnimation(0);
 }
 
-void ModelAnimator::Update(const SceneContext& /*sceneContext*/)
+void ModelAnimator::Update(const SceneContext& sceneContext)
 {
-	TODO_W7_();
 
 	//We only update the transforms if the animation is running and the clip is set
 	if (m_IsPlaying && m_ClipSet)
@@ -17,7 +16,9 @@ void ModelAnimator::Update(const SceneContext& /*sceneContext*/)
 		//1. 
 		//Calculate the passedTicks (see the lab document)
 		//auto passedTicks = ...
-		//Make sure that the passedTicks stay between the m_CurrentClip.Duration bounds (fmod)
+		//Make sure that the passedTicks stay between the m_CurrentClip.Duration bounds (fmod)r
+		auto passedTicks = sceneContext.pGameTime->GetElapsed() * m_CurrentClip.ticksPerSecond * m_AnimationSpeed;
+		passedTicks = fmod(passedTicks, m_CurrentClip.duration);
 
 		//2. 
 		//IF m_Reversed is true
@@ -26,6 +27,18 @@ void ModelAnimator::Update(const SceneContext& /*sceneContext*/)
 		//ELSE
 		//	Add passedTicks to m_TickCount
 		//	if m_TickCount is bigger than the clip duration, subtract the duration from m_TickCount
+		if (m_Reversed)
+		{
+			m_TickCount -= passedTicks;
+			if (m_TickCount < 0) m_TickCount += m_CurrentClip.duration;
+		}
+		else
+		{
+			m_TickCount += passedTicks;
+			if (m_TickCount > m_CurrentClip.duration) m_TickCount -= m_CurrentClip.duration;
+		}
+
+
 
 		//3.
 		//Find the enclosing keys
@@ -33,6 +46,27 @@ void ModelAnimator::Update(const SceneContext& /*sceneContext*/)
 		//Iterate all the keys of the clip and find the following keys:
 		//keyA > Closest Key with Tick before/smaller than m_TickCount
 		//keyB > Closest Key with Tick after/bigger than m_TickCount
+
+		keyA = m_CurrentClip.keys.at(0);
+		for (auto key : m_CurrentClip.keys)
+		{
+			auto tempKey = key;
+			if (tempKey.tick > m_TickCount)
+				break;
+
+			keyA = tempKey;
+		}
+
+		for (auto key : m_CurrentClip.keys)
+		{
+			if (key.tick > m_TickCount)
+			{
+				keyB = key;
+				break;
+			}
+		}
+
+
 
 		//4.
 		//Interpolate between keys
@@ -47,12 +81,55 @@ void ModelAnimator::Update(const SceneContext& /*sceneContext*/)
 		//	Lerp between all the transformations (Position, Scale, Rotation)
 		//	Compose a transformation matrix with the lerp-results
 		//	Add the resulting matrix to the m_Transforms vector
+
+		float tickDiff = keyB.tick = keyA.tick;
+		float blendFactor = (m_TickCount - keyA.tick) / tickDiff;
+
+
+
+		m_Transforms.clear();
+
+		for (int i{}; i < keyA.boneTransforms.size(); ++i)
+		{
+			auto transformA = XMLoadFloat4x4(&keyA.boneTransforms[i]);
+			auto transformB = XMLoadFloat4x4(&keyB.boneTransforms[i]);
+
+			XMVECTOR posA{};
+			XMVECTOR posB{};
+
+			XMVECTOR rotA{};
+			XMVECTOR rotB{};
+
+			XMVECTOR scaleA{};
+			XMVECTOR scaleB{};
+
+			XMMatrixDecompose(&posA, &rotA, &scaleA, transformA);
+			XMMatrixDecompose(&posB, &rotB, &scaleB, transformB);
+
+			XMVECTOR pos = XMVectorLerp(posA, posB, blendFactor);
+			XMVECTOR rot = XMVectorLerp(rotA, rotB, blendFactor);
+			XMVECTOR scale =  XMVectorLerp(scaleA, scaleB, blendFactor);
+
+			XMMATRIX transform =  XMMatrixAffineTransformation(scale, XMVectorZero(), rot, pos);
+			XMFLOAT4X4 finalTransform{};
+			XMStoreFloat4x4(&finalTransform, transform);
+
+			m_Transforms.push_back(finalTransform);
+
+
+		}
+
+
+		return;
+
+
+
 	}
 }
 
-void ModelAnimator::SetAnimation(const std::wstring& /*clipName*/)
+void ModelAnimator::SetAnimation(const std::wstring& clipName)
 {
-	TODO_W7_()
+	TODO_W7_();
 	//Set m_ClipSet to false
 	//Iterate the m_AnimationClips vector and search for an AnimationClip with the given name (clipName)
 	//If found,
@@ -60,11 +137,25 @@ void ModelAnimator::SetAnimation(const std::wstring& /*clipName*/)
 	//Else
 	//	Call Reset
 	//	Log a warning with an appropriate message
+
+	m_ClipSet = false;
+	for (AnimationClip clip : m_pMeshFilter->m_AnimationClips)
+	{
+		if (clipName == clip.name)
+		{
+			SetAnimation(clip);
+			return;
+		}
+	}
+
+	Reset();
+	Logger::LogWarning(L"no clip with name {} could be found ", clipName);
+
+
 }
 
-void ModelAnimator::SetAnimation(UINT /*clipNumber*/)
+void ModelAnimator::SetAnimation(UINT clipNumber)
 {
-	TODO_W7_()
 	//Set m_ClipSet to false
 	//Check if clipNumber is smaller than the actual m_AnimationClips vector size
 	//If not,
@@ -74,20 +165,36 @@ void ModelAnimator::SetAnimation(UINT /*clipNumber*/)
 	//else
 		//	Retrieve the AnimationClip from the m_AnimationClips vector based on the given clipNumber
 		//	Call SetAnimation(AnimationClip clip)
+
+	m_ClipSet = false;
+	if (clipNumber >= m_pMeshFilter->m_AnimationClips.size())
+	{
+		Reset(true);
+		Logger::LogWarning(L"clip number exceeds the amount of available clips in the mesh filter in mesh {}", m_pMeshFilter->m_MeshName);
+		return;
+	}
+	else
+	{
+		AnimationClip clip = m_pMeshFilter->m_AnimationClips[clipNumber];
+		SetAnimation(clip);
+	}
+
+
 }
 
-void ModelAnimator::SetAnimation(const AnimationClip& /*clip*/)
+void ModelAnimator::SetAnimation(const AnimationClip& clip)
 {
-	TODO_W7_()
 	//Set m_ClipSet to true
 	//Set m_CurrentClip
 
+	m_ClipSet = true;
+	m_CurrentClip = clip;
+	Reset(false);
 	//Call Reset(false)
 }
 
-void ModelAnimator::Reset(bool /*pause*/)
+void ModelAnimator::Reset(bool pause)
 {
-	TODO_W7_()
 	//If pause is true, set m_IsPlaying to false
 
 	//Set m_TickCount to zero
@@ -99,4 +206,23 @@ void ModelAnimator::Reset(bool /*pause*/)
 	//Else
 	//	Create an IdentityMatrix 
 	//	Refill the m_Transforms vector with this IdenityMatrix (Amount = BoneCount) (have a look at vector::assign)
+
+	if (pause) m_IsPlaying = false;
+
+	m_TickCount = 0;
+	m_AnimationSpeed = 1.f;
+
+	if (m_ClipSet == true)
+	{
+		m_Transforms = m_CurrentClip.keys.at(0).boneTransforms;
+	}
+	else
+	{
+		XMMATRIX identity = XMMatrixIdentity();
+		XMFLOAT4X4 iMatrix{};
+		XMStoreFloat4x4(&iMatrix, identity);
+		std::fill(m_Transforms.begin(), m_Transforms.end(), iMatrix);
+	}
+
+
 }
